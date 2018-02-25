e4bfd9a681cad8d01144fdbce3cbcbe20f5b38c1
Steffen Stenersen <steffen.stenersen@gmail.com>
2018-02-25 10:48:37 +0100

    Add config and logging

--- src/main/kotlin/pkg/Application.kt
+++ src/main/kotlin/pkg/Application.kt
@@ -1,11 +1,32 @@
 package pkg
 
+import com.natpryce.konfig.*
+import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
+import mu.KotlinLogging
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 class Application
 
+object server : PropertyGroup() {
+    val port by intType
+    val host by stringType
+}
+
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>) {
+
+    val config = systemProperties() overriding
+            EnvironmentVariables() overriding
+            ConfigurationProperties.fromResource("application.properties")
+
+    logger.info { config.get(server.port) }
+    logger.info { config.get(server.host) }
+
+    logger.info { "---------------------------------------------"  }
+
+
     SpringApplication.run(Application::class.java, *args)
 }
--- src/main/kotlin/pkg/GreetingController.kt
+++ src/main/kotlin/pkg/GreetingController.kt
@@ -1,9 +1,14 @@
 package pkg
 
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
+
+
+private val logger = KotlinLogging.logger {}
+
 
 @RestController
 class GreetingController {
@@ -11,7 +16,10 @@ class GreetingController {
     val counter = AtomicLong()
 
     @GetMapping("/greeting")
-    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) =
-            Greeting(counter.incrementAndGet(), "Hello, $name")
+    fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String) : Greeting {
+        logger.info { "Greeting called, called before: {}"  }
+        logger.info("Greeting called, num: {}", counter.get())
+        return Greeting(counter.incrementAndGet(), "Hello, $name")
+    }
 
 }
