082f4d6819a99b717c37a21d6086555b87935741
Steffen Stenersen <steffen.stenersen@gmail.com>
2018-03-05 20:36:48 +0100

    Runnable fat jar

--- src/main/kotlin/pkg/Application.kt
+++ src/main/kotlin/pkg/Application.kt
@@ -1,8 +1,5 @@
 package pkg
 
-import com.natpryce.konfig.*
-import mu.KotlinLogging
-import org.jetbrains.exposed.sql.Table
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.transaction.annotation.EnableTransactionManagement
@@ -11,33 +8,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement
 @EnableTransactionManagement
 class Application
 
-
-
-object server : PropertyGroup() {
-    val port by intType
-    val host by stringType
-}
-
-private val logger = KotlinLogging.logger {}
-
 fun main(args: Array<String>) {
-
-    // val test = Application::javaClass.javaClass.classLoader.getResource("application.properties")
-
-    // val path = args[0]
-
-    // val path2 = System.getProperty("user.dir")
-
-//    val config = systemProperties() overriding
-//            EnvironmentVariables() overriding
-//            ConfigurationProperties.fromResource(path2 + "/resources/application.properties")       //     ConfigurationProperties.fromResource(test.path)
-
-
-//    logger.info { config.get(server.port) }
-//    logger.info { config.get(server.host) }
-
-    logger.info { "---------------------------------------------"  }
-
-
     SpringApplication.run(Application::class.java, *args)
 }
+
